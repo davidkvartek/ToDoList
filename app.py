641d7d03909e9b7d1e1ac318e74041203ab1ca9e
David Kvartek <davidkvartek@gmail.com>
2026-01-20 13:02:48 -0500

    Enhance app with persistence, styling, and task management

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
-# app.py
-from flask import Flask, render_template_string, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-todos = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    complete = db.Column(db.Boolean, default=False)
+
+with app.app_context():
+    db.create_all()
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        todos.append(request.form["todo"])
-        return redirect("/")
-    return render_template_string("""
-        <h1>To-Do List</h1>
-        <form method="post">
-            <input name="todo" placeholder="Add a to-do">
-            <button type="submit">Add</button>
-        </form>
-        <ul>
-            {% for todo in todos %}
-                <li>{{ todo }}</li>
-            {% endfor %}
-        </ul>
-    """, todos=todos)
+        title = request.form.get("todo")
+        if title:
+            new_todo = Todo(title=title)
+            db.session.add(new_todo)
+            db.session.commit()
+        return redirect(url_for('index'))
+    
+    # Show incomplete items first, then completed items
+    todos = Todo.query.order_by(Todo.complete, Todo.id.desc()).all()
+    return render_template("index.html", todos=todos)
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    todo = Todo.query.get_or_404(todo_id)
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run(debug=True)
